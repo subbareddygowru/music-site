82a8e6faa6dcd36be8d9d9cbce4d5fd935d9d4c7
SubbaReddy Gowru <gvsreddy93@gmail.com>
2026-07-09 16:26:25 +0530

    Refactor DOMContentLoaded event listener
    Removed old header and footer loading code.

--- js/include.js
+++ js/include.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function ()
-{
-    // Header //
-    fetch("/includes/header.html")
-        .then(response => response.text())
-        .then(data =>
-        {
-            document.getElementById("header").innerHTML = data;
-        });
-
-    // Footer
-    fetch("/includes/footer.html")
-        .then(response => response.text())
-        .then(data =>
-        {
-            document.getElementById("footer").innerHTML = data;
-        });
-
-});
-
 document.addEventListener("DOMContentLoaded", function () {
 
     // Load Header
